2b20adbb411e1ee3ae1e34da389c786c2447230b
SANTISOFT <santimasoft@gmail.com>
2024-02-21 14:34:34 +0200

    change name

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,3 +26,7 @@ function scrollHeader() {
     if (this.scrollY >= 80) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
+
+
+// functiion for middle function
+// prodcuct  list and search botton

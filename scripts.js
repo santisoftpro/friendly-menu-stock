f85cf9c18a127316cb0ec36a2c7f19f381a51313
santisoftpro <santimasoft@gmail.com>
2024-07-31 19:46:18 +0200

    sales pages

---
+++ scripts.js
@@ -0,0 +1,16 @@
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = "block";
+}
+
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = "none";
+}
+
+window.onclick = function(event) {
+    const modals = document.getElementsByClassName('modal');
+    for (let i = 0; i < modals.length; i++) {
+        if (event.target == modals[i]) {
+            modals[i].style.display = "none";
+        }
+    }
+}
